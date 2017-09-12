e23a5a790947bfad77d8e7d845c73082bf33ab61
Nina Lobe <nina.lobe@gmail.com>
2017-09-12 21:31:39 +0200

    ostali del projekta

---
+++ pretvornik_crk.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+print "pretvori v male črke"
+stavek = raw_input("Vpiši besedilo, če želiš, da bodo samo male črke: ")
+stavek = stavek.lower()
+print stavek
